b3bc27eb3f75198d2d2284d4594bb785dba1fb04
Saint-M <saint-martin_10@hotmail.com>
2022-07-19 15:11:05 -0500

    agregamos estilos a navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,29 +2,34 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+    <>
+      <nav className="navbar navbar-expand-lg shadow">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Robotech</a>
+          <a href="#" className="navbar-brand px-4 px-lg-5">
+            <h1 className="h2 m-0">SmartFit</h1>
+          </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Nosotros</a>
+                <a className="nav-link active" aria-current="page" href="#">Rutinas</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contacto</a>
+                <a className="nav-link" href="#">Recetas</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#"></a>
+                <a className="nav-link" href="#">Nosotros</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Guardados <i className="bi bi-bookmark"></i></a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-    </div>
+    </>
   )
 }
 
