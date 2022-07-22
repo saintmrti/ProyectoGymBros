970c58c6863c295cb35f5ee2d637d1feec699cc6
Saint-M <saint-martin_10@hotmail.com>
2022-07-22 00:48:59 -0500

    cambiamos a tailwind

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,30 +3,20 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg shadow">
-        <div className="container-fluid">
-          <a href="#" className="navbar-brand px-4 px-lg-5">
-            <h1 className="h2 m-0">SmartFit</h1>
-          </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Rutinas</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Recetas</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Nosotros</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Guardados <i className="bi bi-bookmark"></i></a>
-              </li>
-            </ul>
-          </div>
+      <nav>
+        <div className='container mx-auto flex'>
+          <h1>GymBros</h1>
+          <ul className='flex'>
+            <li>
+              <a href="">Rutinas</a>
+            </li>
+            <li>
+              <a href="">Recetas</a>
+            </li>
+            <li>
+              <a href="">Nosotros</a>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
