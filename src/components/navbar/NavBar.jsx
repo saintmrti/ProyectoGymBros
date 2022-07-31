b47a897f4276b120219db79429ccfec1ae39e183
Saint-M <saint-martin_10@hotmail.com>
2022-07-31 15:56:25 -0500

    modificamos estilos en navbar y agregamos nueva funcionalidad

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   function openMenu() {
     let menu = document.getElementById('menu')
 
-    menu.classList.contains('hidden') ? menu.classList.remove('hidden') : menu.classList.add('hidden')
+    menu.classList.contains('show') ? menu.classList.remove('show') : menu.classList.add('show')
   }
 
   return (
@@ -16,15 +16,15 @@ const NavBar = () => {
           <div className='flex lg:hidden'>
             <button onClick={openMenu}><i id='iconNav' className='bi bi-list'></i></button>
           </div>
-          <div id='menu' className='lg:flex hidden flex-grow justify-between items-center absolute lg:relative lg:top-0 top-20 w-full lg:w-auto left-0 p-7 lg:p-0'>
-            <ul className='flex flex-col lg:flex-row'>
-              <li className='mr-3 mb-5 lg:mb-0'>
+          <div className='lg:flex flex-grow justify-between items-center absolute lg:relative lg:top-0 top-20 w-full lg:w-auto left-0 p-7 lg:p-0'>
+            <ul id='menu' className='flex flex-col lg:flex-row'>
+              <li className='mr-3 mb-10 mt-10 lg:mb-0 lg:mt-0'>
                 <a href="">Rutinas</a>
               </li>
-              <li className='mr-3 mb-5 lg:mb-0'>
+              <li className='mr-3 mb-10 lg:mb-0'>
                 <a href="">Recetas</a>
               </li>
-              <li className='mb-5 lg:mb-0'>
+              <li className='mb-10 lg:mb-0'>
                 <a href="">Nosotros</a>
               </li>
               <li className='visible lg:invisible'>
