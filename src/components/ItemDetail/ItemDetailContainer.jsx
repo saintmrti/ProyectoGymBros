e079bc6859173410e9122929fe47fd653ddb2da4
Saint-M <saint-martin_10@hotmail.com>
2022-08-03 13:43:00 -0500

    actualizamos funciones, cambios en itemDetailContainer

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -9,14 +9,7 @@ const ItemDetailContainer = () => {
   useEffect(() => {
     let detailsArray =[
         {id:1, nombre:"Creatina", marca:"RC", precio: 250, descripcion: `
-            sabor: sin sabor
-            Contenido: 1000 gr
-            Porciones:200
-            
-            PREGUNTA POR LOS SABORES DISPONIBLES
-            
-            ESTE PRODUCTO NO ES UN MEDICAMENTO
-            EL CONSUMO DE ESTE PRODUCTO ES RESPONSABILIDAD DE QUIEN LO RECOMIENDA Y DE QUIEN LO USA
+            sabor: sin sabor Contenido: 1000 gr Porciones:200 PREGUNTA POR LOS SABORES DISPONIBLES ESTE PRODUCTO NO ES UN MEDICAMENTO EL CONSUMO DE ESTE PRODUCTO ES RESPONSABILIDAD DE QUIEN LO RECOMIENDA Y DE QUIEN LO USA
         `},
         {id:2, nombre:"Proteina", marca:"Whey", precio: 1500, descripcion: `
             Proteína aislada de suero de leche optimum nutrition - gold standard whey sabor vainilla. Bote de 2 lb. Este producto fue creado con atención al detalle, para que el usuario pueda sentir la calidad óptima desde el primer segundo. Un equilibrio óptimo entre las necesidades del cliente y un merecido mimo.
@@ -25,7 +18,7 @@ const ItemDetailContainer = () => {
 
     const promesaDetails = new Promise((res, rej) => {
         setTimeout(() => {
-          res(detailsArray.filter((item) => item.id === parseInt(idProducto)))
+          res(detailsArray.find((item) => item.id === parseInt(idProducto)))
         }, 2000)
     })
 
@@ -35,9 +28,7 @@ const ItemDetailContainer = () => {
   }, [idProducto])
   return (
     <>
-        {details.map((item)=>
-            <ItemDetail key={item.id} nombre={item.nombre} marca={item.marca} precio={item.precio} descripcion={item.descripcion}/>
-        )}
+        <ItemDetail details={details}/>
     </>
   )
 }
