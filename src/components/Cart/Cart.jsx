8fbe1746bdc07dbfe665f2eae798ded99771317d
Saint-M <saint-martin_10@hotmail.com>
2022-08-21 13:53:38 -0500

    hicimos cambios en Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,19 +6,21 @@ import { useContext } from 'react';
 import { myContext } from './CartContext';
 
 const Cart = () => {
-
   const [showBtn, setShowBtn] = useState(false)
   const {save} = useContext(myContext)
   
   useEffect(()=>{
-    save.length > 0 && setShowBtn(true)
-  }, [])
+    save.length > 0 ? setShowBtn(false) : setShowBtn(true)
+  }, [save])
 
   
   return (
     <>
         <div className="flex flex-grow justify-center">
-            {showBtn ? <SaveList elements={save}/> : <button className="bg-yellow-400 py-2 px-3 rounded-md text-white font-bold mt-5 hover:bg-yellow-500"><Link to={"/"}>Ir a comprar</Link></button>}
+            {showBtn ? <div>
+              <p className="text-xl mt-10">Lo sentimos ): <br /> No tienes nada en tu carrito</p>
+              <button className="bg-yellow-400 py-2 px-3 rounded-md text-white font-bold mt-3 hover:bg-yellow-500"><Link to={"/"}>Ir a comprar</Link></button>
+            </div> : <SaveList elements={save}/>}
         </div>
     </>
   )
